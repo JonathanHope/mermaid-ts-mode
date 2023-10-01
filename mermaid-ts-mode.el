3f3a537d249b44e939d6a0d65a6c316761ff8c41
Jonathan Hope <jhope@fastmail.com>
2023-10-01 10:04:01 -0700

    Removing a spurious license line.

--- mermaid-ts-mode.el
+++ mermaid-ts-mode.el
@@ -8,8 +8,6 @@
 ;; Package-Requires: ((emacs "29.1"))
 ;; Homepage: https://github.com/JonathanHope/mermaid-ts-mode
 
-;; This file is part of GNU Emacs.
-
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
